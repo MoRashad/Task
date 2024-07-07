8a7fae640db0b3f68d1fc26baf5bc51b853fc3c3
MoRashad <44236713+MoRashad@users.noreply.github.com>
2024-07-07 17:04:05 +0300

    add: - events post request - events get request controllers

--- server/src/controller/events.controller.ts
+++ server/src/controller/events.controller.ts
@@ -1,16 +1,68 @@
 import { Request, Response } from "express";
-import { EventsService } from "../service/events.service";
-
+import * as utils from "../utils";
+import { EventBody, EventsQueryParams } from "../types/types";
+import { CustomError } from "../error";
+import { Event, Prisma } from "@prisma/client";
+import { createEvent, getEventsCount, getPaginatedEvents } from "../service/events.service";
 export class EventsController {
-	private eventsService: EventsService;
-	constructor(eventService: EventsService) {
-		this.eventsService = eventService;
-	}
 	public async createEvent(req: Request, res: Response) {
-		res.status(201).json({ message: "Event created" });
+		const requestBody: EventBody = req.body;
+		const actorDetails = utils.getActor(requestBody.actor_id);
+		const targetDetails = utils.getActor(requestBody.trget_id);
+		const getEventDetails = utils.getAction(requestBody.action_id);
+		if (!actorDetails || !targetDetails || !getEventDetails) {
+			throw new CustomError({
+				message: "Invalid data",
+				code: 400,
+				status: "error",
+			});
+		}
+		const data: Prisma.EventCreateInput = {
+			id: utils.generateUniqueId(),
+			action_id: getEventDetails.id,
+			action_name: getEventDetails.name,
+			actor_id: actorDetails.actor_id,
+			actor_name: actorDetails.name,
+			group: actorDetails.group,
+			object: "event",
+			metadata: requestBody.metaData,
+			location: req.socket.remoteAddress,
+			occurred_at: new Date(requestBody.date),
+			target_id: targetDetails.actor_id,
+			target_name: targetDetails.name,
+		};
+		console.log("🚀 ~ EventsController ~ createEvent ~ data:", data);
+		const event = await createEvent(data);
+		console.log("🚀 ~ EventsController ~ createEvent ~ event:", event);
+		res.status(201).json({ message: "Event created", data });
 	}
 
 	public async getEvents(req: Request, res: Response) {
-		res.status(200).json({ message: "Events retrieved" });
+		const query: EventsQueryParams = req.query;
+		const page = query.page || 1;
+		const limit = query.limit || 1;
+		const search = query.search || "";
+		const totalEvents = await getEventsCount(search);
+		const events = await getPaginatedEvents({ page, pageSize: limit, search });
+		const totalPage = Math.ceil(totalEvents / limit);
+		const lastPage = totalPage === 0 ? 1 : totalPage;
+		const isLastPage = page === lastPage;
+		const result: {
+			events: Event[];
+			totalEvents: number;
+			totalPage: number;
+			page: number;
+			lastPage: number;
+			isLastPage: boolean;
+		} = {
+			events: events,
+			totalEvents,
+			totalPage,
+			page,
+			lastPage,
+			isLastPage,
+		};
+
+		res.status(200).json(result);
 	}
 }
